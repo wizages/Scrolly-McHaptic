1093b84052addd01de69ef65ca76974668644ace
wizage <wizagesmax@gmail.com>
2017-02-14 03:05:46 -0700

    Added icons

--- scrollyprefs/smcprefRootListController.m
+++ scrollyprefs/smcprefRootListController.m
@@ -1,5 +1,7 @@
 #include "smcprefRootListController.h"
 #import <CepheiPrefs/HBSupportController.h>
+//#import <CepheiPrefs/HBRespringController.h>
+
 
 @implementation smcprefRootListController
 
@@ -18,6 +20,14 @@
 - (void)showSupportEmailController {
 	UIViewController *viewController = (UIViewController *)[HBSupportController supportViewControllerForBundle:[NSBundle bundleForClass:self.class] preferencesIdentifier:@"com.wizage.scrollymchaptic"];
 	[self.navigationController pushViewController:viewController animated:YES];
+}
+
+-(void)respring{
+	//[HBRespringController respring];
+	#pragma clang diagnostic push
+	#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+	    system("killall -9 SpringBoard");
+	#pragma clang diagnostic pop
 }
 
 @end
